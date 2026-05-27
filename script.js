e7f1abbcb232083e348e1cc37369fdf9447e1ce0
sonjabatiste-star <sonja.batiste@gmail.com>
2026-05-27 14:37:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function openLesson(week) {
+    alert("This will open Week " + week + " lesson content. We can add full lessons later!");
+}
 
+document.getElementById("progressText").innerText =
+    "Zoey has completed 1 out of 12 lessons. Great start!";
